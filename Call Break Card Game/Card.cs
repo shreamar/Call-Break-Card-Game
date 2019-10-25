073bc574f041fb04b269bc575af0be2d85ad365b
Amar Shrestha <shreamar@isu.edu>
2019-10-24 20:03:48 -0600

    Card class and Deck class created

---
+++ Call Break Card Game/Card.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_Break_Card_Game
+{
+    public class Card
+    {
+        private CardNumber _Number;
+        private CardSuit _Suit;
+        private bool _Thrown = false;
+
+        public Card()
+        {
+            _Number = CardNumber.Two;
+            _Suit = CardSuit.Club;
+        }
+
+        public Card(CardNumber number, CardSuit suit)
+        {
+            _Number = number;
+            _Suit = suit;
+        }
+
+        public CardNumber Number
+        {
+            get { return _Number; }
+            set { _Number = value; }
+        }
+
+        public CardSuit Suit
+        {
+            get { return _Suit; }
+            set { _Suit = value; }
+        }
+
+        public int ID
+        {
+            get { return ((int)Suit * 10) + (int)Number; }        //Two-Club is 0, Ace-Spade is 51
+        }
+
+        public bool Thrown
+        {
+            get { return _Thrown; }
+            set { _Thrown = value; }
+        }
+
+        /// <summary>
+        /// Displays the name of the given card
+        /// For example, 9-Heart
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                string name = "";
+
+                //since Two = 0, 9 = Jack
+                if ((int)Number < 9)
+                {
+                    name = ((int)Number+2).ToString();
+                }
+                else if ((int)Number >= 9)
+                {
+                    //picks only the first letter of face/ace cards
+                    name = Number.ToString()[0].ToString();
+                }
+
+                return name + "-" + Suit.ToString();
+            }
+        }
+
+        /// <summary>
+        /// no specific order except Spade is trump card with highest index value
+        /// </summary>
+        public enum CardSuit { Club, Diamond, Heart, Spade}; 
+
+        /// <summary>
+        /// 2 is lowest value where as Ace is higher than King
+        /// </summary>
+        public enum CardNumber { Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+        
+
+    }
+}
---
+++ Call Break Card Game/Deck.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_Break_Card_Game
+{
+    class Deck
+    {
+        private Card[] _Cards;
+        private bool _Shuffled = false;
+
+        public Deck()
+        {
+            _Cards = new Card[52];
+            createCards();
+            shuffleDeck();
+        }
+
+        public Card[] Cards
+        {
+            get { return _Cards; }
+            set { _Cards = value; }
+        }
+
+        public bool Shuffled
+        {
+            get { return _Shuffled; }
+        }
+
+        /// <summary>
+        /// Creates the deck of cards
+        /// </summary>
+        private void createCards()
+        {
+            int counter = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    Cards[counter].Number = (Card.CardNumber)i;
+                    Cards[counter].Suit = (Card.CardSuit)j;
+                    counter++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shuffles the deck of cards
+        /// </summary>
+        private void shuffleDeck()
+        {
+            //create first random number as seed
+            Random random = new Random();
+            int rnd = random.Next(1, 999999999);
+
+            for (int i = 0; i < 52; i++)
+            {
+                //updates the seeds everytime so they are pseudorandom and all are not same
+                Random random1 = new Random(rnd + i);
+                int rndIndex = random1.Next(52);
+
+                swapCards(Cards[i], Cards[rndIndex]);
+            }
+            _Shuffled = true;
+        }
+
+        private void swapCards(Card a, Card b)
+        {
+            Card temp = new Card();
+            temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
